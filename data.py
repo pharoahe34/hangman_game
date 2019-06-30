f29b38129fc1a333cec296e0cba98b19613dd9fa
Marc Muckensturm <muckensturm.marc@gmail.com>
2019-06-30 21:49:22 +0200

    initial commit

---
+++ data.py
@@ -0,0 +1,14 @@
+"""This files defined few data for the hangman game """
+# settings
+# liste of words for the game
+words_to_find = [
+    "football",
+    "victoire",
+    "glass",
+    "hello",
+    "gorgeous"
+]
+# number of try
+numbers_of_try = 8
+# dictionary of player
+scores_file = "scores"
