93c5889da43b8fc131050155d1514b3ebdc9a5a5
Marc Muckensturm <muckensturm.marc@gmail.com>
2020-05-15 15:42:04 +0200

    formatting python code

--- hangman_game.py
+++ hangman_game.py
@@ -4,8 +4,8 @@ It's based on the files:
 - data.py
 - functions.py"""
 
-from data import *
-from functions import *
+from data import numbers_of_try
+from functions import retrieve_scores, retrieve_user_name, choose_a_word, retrieve_hidden_word, retrieve_letter, save_scores
 
 # scores of the game
 scores = retrieve_scores()
@@ -18,20 +18,20 @@ if user not in scores.keys():
     scores[user] = 0
 
 # when to stop the game
-continue_game = 'o'
+continue_game = "o"
 
-while continue_game != 'n':
+while continue_game != "n":
     print("Player {0}: {1} points!".format(user, scores[user]))
     words_to_find = choose_a_word()
     find_letters = []
     find_word = retrieve_hidden_word(words_to_find, find_letters)
     numb_chances = numbers_of_try
-    while words_to_find!=find_word and numb_chances>0:
+    while words_to_find != find_word and numb_chances > 0:
         print("Word to find {0} (chance {1} left)".format(words_to_find, numb_chances))
         letter = retrieve_letter()
-        if letter in find_letters: # letter already choose
+        if letter in find_letters:  # letter already choose
             print("letter already choosen.")
-        elif letter in words_to_find: # letter is in the word to find
+        elif letter in words_to_find:  # letter is in the word to find
             find_letters.append(letter)
             print("good game!")
         else:
@@ -39,7 +39,7 @@ while continue_game != 'n':
             print("try again...")
         find_word = retrieve_hidden_word(words_to_find, find_letters)
 
-# is it game over?
+    # is it game over?
     if words_to_find == find_word:
         print("GG! you find the word {0}.".format(words_to_find))
     else:
