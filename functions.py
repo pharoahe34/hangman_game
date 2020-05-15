2ffd3b2d1fea5da22801223701760f13560d4c84
Marc Muckensturm <muckensturm.marc@gmail.com>
2020-05-15 15:17:30 +0200

    formatting python code

--- functions.py
+++ functions.py
@@ -3,14 +3,15 @@ import os
 import pickle
 import time
 import random
-from data import *
+from data import my_scores_file, words_list
 
 # score management
+
 
 def retrieve_scores():
     """This function store the scores in a dictionary"""
 
-    if os.path.exists(my_scores_file): #folder exist
+    if os.path.exists(my_scores_file):  # folder exist
         scores_file = open(my_scores_file, "rb")
         depickler = pickle.Unpickler(scores_file)
         scores = depickler.load()
@@ -19,15 +20,18 @@ def retrieve_scores():
         scores = {}
     return scores
 
+
 def save_scores(scores):
     """ This function is in charge to save scores in my_scores_file"""
 
-    scores_file = open(my_scores_file, "wb") # On écrase les anciens scores
+    scores_file = open(my_scores_file, "wb")  # On écrase les anciens scores
     pickler = pickle.Pickler(scores_file)
     pickler.dump(scores)
     scores_file.close()
 
+
 # user's inputs
+
 
 def retrieve_user_name():
     """This function must retrieve the name of the user.
@@ -36,7 +40,7 @@ def retrieve_user_name():
     user_name = input("What is your name? ")
     # fisrt lettre in capital and the others in lower case
     user_name = user_name.capitalize()
-    if not user_name.isalnum() or len(user_name)<4:
+    if not user_name.isalnum() or len(user_name) < 4:
         print("Invalid name")
         # we call the function again
         return retrieve_user_name()
@@ -52,7 +56,7 @@ def retrieve_letter():
 
     letter = input("Chose a letter: ")
     letter = letter.lower()
-    if len(letter)>1 or not letter.isalpha():
+    if len(letter) > 1 or not letter.isalpha():
         print("You don't enter a correct input.")
         return retrieve_letter
     else:
@@ -60,12 +64,15 @@ def retrieve_letter():
         time.sleep(1)
         return letter
 
+
 # game function
+
 
 def choose_a_word():
     """This function returns the word chossen in the words_to_find list"""
 
     return random.choice(words_list)
+
 
 def retrieve_hidden_word(full_word, found_letter):
     """This function returns a hidden word. 
