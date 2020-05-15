54b440b164b9b8ed40a7ad082643f71b91551c40
Marc Muckensturm <muckensturm.marc@gmail.com>
2020-05-15 15:14:31 +0200

    formatting python code

--- data.py
+++ data.py
@@ -1,13 +1,7 @@
 """This files defined few data for the hangman game """
 # settings
 # liste of words for the game
-words_list = [
-    "football",
-    "victory",
-    "glass",
-    "hello",
-    "gorgeous"
-]
+words_list = ["football", "victory", "glass", "hello", "gorgeous"]
 # number of try
 numbers_of_try = 8
 # file that keep a backup of scores
