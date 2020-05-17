0d6375844c0b7aee800229f5f875611d089640fe
Marc Muckensturm <muckensturm.marc@gmail.com>
2020-05-17 20:21:41 +0200

    changed constant name in capital letter

--- hangman_game.py
+++ hangman_game.py
@@ -5,54 +5,55 @@ It's based on the files:
 - functions.py"""
 
 from data import numbers_of_try
-from functions import retrieve_scores, retrieve_user_name, choose_a_word, retrieve_hidden_word, retrieve_letter, save_scores
+from functions import retrieve_scores, retrieve_user_name, choose_a_word
+from functions import retrieve_hidden_word, retrieve_letter, save_scores
 
 # scores of the game
-scores = retrieve_scores()
+SCORES = retrieve_scores()
 
 # name of the player
-user = retrieve_user_name()
+USER = retrieve_user_name()
 
 # if user do not exist we add him
-if user not in scores.keys():
-    scores[user] = 0
+if USER not in SCORES.keys():
+    SCORES[USER] = 0
 
 # when to stop the game
-continue_game = "o"
+CONTINUE_GAME = "o"
 
-while continue_game != "n":
-    print("Player {0}: {1} points!".format(user, scores[user]))
-    words_to_find = choose_a_word()
-    find_letters = []
-    find_word = retrieve_hidden_word(words_to_find, find_letters)
-    numb_chances = numbers_of_try
-    while words_to_find != find_word and numb_chances > 0:
-        print("Word to find {0} (chance {1} left)".format(words_to_find, numb_chances))
-        letter = retrieve_letter()
-        if letter in find_letters:  # letter already choose
+while CONTINUE_GAME != "n":
+    print(f"Player {USER}: {SCORES[USER]} points!")
+    WORDS_TO_FIND = choose_a_word()
+    FIND_LETTER = []
+    FIND_WORD = retrieve_hidden_word(WORDS_TO_FIND, FIND_LETTER)
+    NUMB_CHANCES = numbers_of_try
+    while WORDS_TO_FIND != FIND_WORD and NUMB_CHANCES > 0:
+        print(f"(chance {NUMB_CHANCES} left)")
+        LETTER = retrieve_letter()
+        if LETTER in FIND_LETTER:  # letter already choose
             print("letter already choosen.")
-        elif letter in words_to_find:  # letter is in the word to find
-            find_letters.append(letter)
+        elif LETTER in WORDS_TO_FIND:  # letter is in the word to find
+            FIND_LETTER.append(LETTER)
             print("good game!")
         else:
-            numb_chances -= 1
+            NUMB_CHANCES -= 1
             print("try again...")
-        find_word = retrieve_hidden_word(words_to_find, find_letters)
+        FIND_WORD = retrieve_hidden_word(WORDS_TO_FIND, FIND_LETTER)
 
     # is it game over?
-    if words_to_find == find_word:
-        print("GG! you find the word {0}.".format(words_to_find))
+    if WORDS_TO_FIND == FIND_WORD:
+        print(f"GG! you find the word {WORDS_TO_FIND}.")
     else:
-        print("Hangover !!! You lose.")
+        print(f"Hangover! You lose.\nThe word to find was:\n{WORDS_TO_FIND}")
 
     # updated user scores
-    scores[user] += numb_chances
+    SCORES[USER] += NUMB_CHANCES
 
-    continue_game = input("You want to continue (O/N) ?")
-    continue_game = continue_game.lower()
+    CONTINUE_GAME = input("You want to continue (O/N) ?")
+    CONTINUE_GAME = CONTINUE_GAME.lower()
 
 # game is over
-save_scores(scores)
+save_scores(SCORES)
 
 # Display the user scores
-print("you finished the game with {0} points.".format(scores[user]))
+print(f"You finished the game with {SCORES[USER]} points.")
